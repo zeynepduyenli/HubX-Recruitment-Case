eb42bbb9b0a244bbf36b6b6ab6511cf4a78a3544
Zeynep Düyenli <duyenlizeynep@gmail.com>
2024-12-04 23:17:59 +0300

    Upgrade existing libraries and set up architecture packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.zows.hubxrecruitmentcase"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.zows.hubxrecruitmentcase"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
